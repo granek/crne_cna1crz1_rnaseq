8ff04599c6ba98769ab425cae091adc811806fd4
Josh Granek <josh@duke.edu>
2014-08-29 13:26:50 -0400

    calcineurin_reg_analysis.R: Removed code specific to older versions of DESeq2: 1. Removed countfiltering, since DESeq2 does this by default. 2. Removed old use of results(name=) in favor of contrasts.

--- calcineurin_reg_analysis.R
+++ calcineurin_reg_analysis.R
@@ -57,23 +57,16 @@ resultsNames(ddsHTSeq)[3:6]
 
 
 
-FindDiffGenes = function(ddsHTSeq,outbase, fdrcutoff=0.05, fccutoff=2,countfilter=FALSE){
-    print(paste("countfilter:", countfilter))
+FindDiffGenes = function(ddsHTSeq,outbase, fdrcutoff=0.05, fccutoff=2){
     ## stop("need to do filtering for each comparison????")
     log2fc = log2(fccutoff)
     # for(var in seq) expr
     ListOfGeneVecs = list()
     for(sample in c("KI_CNA1","KI_CRZ1","KO_cna1","KO_crz1")) {
-        if (packageVersion("DESeq2")=="1.2.10"){
-            coeff = paste("condition",sample, "vs_WT",sep="_")
-            outfile = paste(sep="_",coeff, "results",fileend)
-            cur.res = results(ddsHTSeq,name=coeff)
-        }else{
-            coeff = paste("condition",sample,sep="")
-            outfile = paste("condition",sample, "vs_WT","results",fileend,sep="_")
-            ## contrast = c("treatment", "DPN", "Control")
-            cur.res = results(ddsHTSeq,contrast = c("condition", sample, "WT"))
-        }
+        coeff = paste("condition",sample,sep="")
+        outfile = paste("condition",sample, "vs_WT","results",fileend,sep="_")
+        ## contrast = c("treatment", "DPN", "Control")
+        cur.res = results(ddsHTSeq,contrast = c("condition", sample, "WT"))
         print(paste("coeff", coeff))
         cur.res = cur.res[order(cur.res$padj),]
         ## print(sample)
@@ -82,26 +75,7 @@ FindDiffGenes = function(ddsHTSeq,outbase, fdrcutoff=0.05, fccutoff=2,countfilte
                   abs(cur.res$log2FoldChange) >= log2fc,
                   dnn=c(paste("FDR<",fdrcutoff), paste("FC>",fccutoff)))
             )
-        if (countfilter) {
-            print("Filtering Genes with mean counts less than 10 or NA pvalue")
-            use <- cur.res$baseMean >= 10 & !is.na(cur.res$pvalue)
-            table(use)
-            resFilt <- cur.res[use,]
-            resFilt$padj <- p.adjust(resFilt$pvalue, method="BH")
-            sum(cur.res$padj < .1, na.rm=TRUE)
-            sum(resFilt$padj < .1, na.rm=TRUE)
-            cur.res = resFilt
-            print(
-                table(cur.res$padj < fdrcutoff,
-                      abs(cur.res$log2FoldChange) >= log2fc,
-                      dnn=c(paste("FDR<",fdrcutoff), paste("FC>",fccutoff)))
-            )
-
-            filtered="_filt"
-        } else {
-            filtered=""
-        }
-        fileend=paste(fdrcutoff*100,"fdr_", fccutoff,"fc",filtered,".csv",sep="")
+        fileend=paste(fdrcutoff*100,"fdr_", fccutoff,"fc",".csv",sep="")
         filt.res = cur.res[which((cur.res$padj < fdrcutoff) &
                           (abs(cur.res$log2FoldChange) >= log2fc)),]
         outfile = paste("condition",sample, "vs_WT","results",fileend,sep="_")
@@ -291,50 +265,43 @@ fdrlist = c(opt$fdr)
 ##------------------------------------------------------------
 filtlist = c(FALSE)
 for (curfc in fclist) {
-    for (countfilter in filtlist){
-        for (curfdr in fdrlist){
-            print(paste("countfilter:", countfilter))
-            if (countfilter) {
-                filtered="_filt"
-            } else {
-                filtered=""
-            }
-            fileend=paste(curfdr*100,"fdr_", curfc,"fc",filtered,sep="")
-            
-            fdg = FindDiffGenes(ddsHTSeq, outbase,fdrcutoff=curfdr,fccutoff=curfc,countfilter=countfilter)
-            KOcna1Genes = fdg[["KO_cna1"]] ## fdg$KOcna1Genes
-            KOcrz1Genes = fdg[["KO_crz1"]] ## fdg$KOcrz1Genes
+    for (curfdr in fdrlist){
+        fileend=paste(curfdr*100,"fdr_", curfc,"fc",sep="")
+        
+        fdg = FindDiffGenes(ddsHTSeq, outbase,fdrcutoff=curfdr,fccutoff=curfc)
+        KOcna1Genes = fdg[["KO_cna1"]] ## fdg$KOcna1Genes
+        KOcrz1Genes = fdg[["KO_crz1"]] ## fdg$KOcrz1Genes
 
-            # ExportNormedCounts(KOcna1Genes,KOcrz1Genes,ddsHTSeq,fileend,outdir)
-            ## AnnotateGeneLists(KOcna1Genes,KOcrz1Genes,fileend,annotdir,outdir)
-            ## ExportResults(KOcna1Genes,KOcrz1Genes,ddsHTSeq,curfdr,curfc,outdir) ##HERE
-            ##-----------------------------------------------------------------
-            ListOfGeneSets = list()
-            ListOfGeneSets[["cna1ko_crz1ko_intersect"]] = intersect(KOcna1Genes,KOcrz1Genes)
-            ListOfGeneSets[["cna1ko_unique"]] = setdiff(KOcna1Genes,KOcrz1Genes)
-            ListOfGeneSets[["crz1ko_unique"]] = setdiff(KOcrz1Genes,KOcna1Genes)
-            GenesOfInterestHeatmap(stack(ListOfGeneSets)$values,ddsHTSeq, 
-                                   outfile= paste(sep="",outbase,paste("goi_heatmap_", fileend,sep="")))
-            ListOfGeneSets[["KI_CRZ1"]] = fdg[["KI_CRZ1"]]
+        # ExportNormedCounts(KOcna1Genes,KOcrz1Genes,ddsHTSeq,fileend,outdir)
+        ## AnnotateGeneLists(KOcna1Genes,KOcrz1Genes,fileend,annotdir,outdir)
+        ## ExportResults(KOcna1Genes,KOcrz1Genes,ddsHTSeq,curfdr,curfc,outdir) ##HERE
+        ##-----------------------------------------------------------------
+        ListOfGeneSets = list()
+        ListOfGeneSets[["cna1ko_crz1ko_intersect"]] = intersect(KOcna1Genes,KOcrz1Genes)
+        ListOfGeneSets[["cna1ko_unique"]] = setdiff(KOcna1Genes,KOcrz1Genes)
+        ListOfGeneSets[["crz1ko_unique"]] = setdiff(KOcrz1Genes,KOcna1Genes)
+        GenesOfInterestHeatmap(stack(ListOfGeneSets)$values,ddsHTSeq, 
+                               outfile= paste(sep="",outbase,paste("goi_heatmap_", fileend,sep="")))
+        ListOfGeneSets[["KI_CRZ1"]] = fdg[["KI_CRZ1"]]
 
-            ExportNormedCounts(rownames(ddsHTSeq),
-                               paste(sep="",outbase,"cna1_crz1_allcounts.csv"),
-                               ddsHTSeq)
-            for (name in names(ListOfGeneSets)) {
-                GenesOfInterestHeatmap(ListOfGeneSets[[name]],ddsHTSeq, 
-                                        outfile= paste(sep="",outbase,paste(name,"_heatmap_", fileend,sep="")))
-                AnnotateGeneLists(ListOfGeneSets[[name]],
-                                  paste(sep="",outbase,paste(name,fileend,"annot.csv",sep="_")),
-                                  annotdir)
-                ## ExportNormedCounts(KOcna1Genes,KOcrz1Genes,ddsHTSeq,fileend,outdir)
-                ## fileend=paste(fileend,"_counts.csv",sep="")
-                ExportNormedCounts(ListOfGeneSets[[name]],
-                                  paste(sep="",outbase,paste(name,fileend,"counts.csv",sep="_")),
-                                  ddsHTSeq)
-            }
+        ExportNormedCounts(rownames(ddsHTSeq),
+                           paste(sep="",outbase,"cna1_crz1_allcounts.csv"),
+                           ddsHTSeq)
+        for (name in names(ListOfGeneSets)) {
+            GenesOfInterestHeatmap(ListOfGeneSets[[name]],ddsHTSeq, 
+                                    outfile= paste(sep="",outbase,paste(name,"_heatmap_", fileend,sep="")))
+            AnnotateGeneLists(ListOfGeneSets[[name]],
+                              paste(sep="",outbase,paste(name,fileend,"annot.csv",sep="_")),
+                              annotdir)
+            ## ExportNormedCounts(KOcna1Genes,KOcrz1Genes,ddsHTSeq,fileend,outdir)
+            ## fileend=paste(fileend,"_counts.csv",sep="")
+            ExportNormedCounts(ListOfGeneSets[[name]],
+                              paste(sep="",outbase,paste(name,fileend,"counts.csv",sep="_")),
+                              ddsHTSeq)
         }
-    }   
-}
+    }
+}   
+
 ###################################################
 ### code chunk number 8: DESeq2_report (eval = FALSE)
 # from http://bioconductor.org/packages/release/bioc/vignettes/ReportingTools/inst/doc/rnaseqAnalysis.R
